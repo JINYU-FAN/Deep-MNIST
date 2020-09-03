97662ace34437e0383a42640ca8a253019d3a6e4
JINYU-FAN <1919304560@qq.com>
2020-09-03 20:11:38 +0800

    FCN trained

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import os
+from models.nn import FullyConnectedNet
 from models.lenet import LeNet 
 from models.vgg import VGG
 from models.resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
@@ -11,19 +14,41 @@ import torch.optim as optim
 import pickle
 from dataset import train_loader
 
-models = [#LeNet(),
+
+'''
+models = [
+          FullyConnectedNet(), LeNet(),
           VGG('VGG11'), VGG('VGG13'), VGG('VGG16'), VGG('VGG19'),
           ResNet18(), ResNet34(), ResNet50(), ResNet101(), ResNet152(),
           DenseNet121(), DenseNet161(), DenseNet169(), DenseNet201()
           ]
+'''
+models = [
+(FullyConnectedNet([784,100,10]), 'FCN_784-100-10'),
+(FullyConnectedNet([784,500,10]), 'FCN_784-500-10'),   
+(FullyConnectedNet([784,100,100,10]), 'FCN_784-100-100-10'),
+(FullyConnectedNet([784,500,500,10]), 'FCN_784-500-500-10'),
+(LeNet(), 'LeNet')     
+]
+
+
+
+
+
 device = 'cuda:0'
-for net in models:
+for net, name in models:
+    if not os.path.exists('./MResults/'+name):
+        os.mkdir('./MResults/'+name)
+    if os.listdir('./MResults/'+name):
+        print(f'Directory {name} is already occupied.')
+        continue
+    start_time = time.time()
     # The path and the name of the model you would like to save
     torch.autograd.set_detect_anomaly(True)
     net.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.95)
-    for epoch in range(1):  # loop over the dataset multiple times
+    for epoch in range(10):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, data in enumerate(train_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
@@ -31,8 +56,6 @@ for net in models:
             # zero the parameter gradients
             optimizer.zero_grad()
             # forward + backward + optimize
-            #if random.random() > 0.5:
-            #    inputs = reverse(inputs)
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -43,11 +66,13 @@ for net in models:
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
+        output_hal = open(f"./MResults/{name}/epoch_{epoch+1}.pkl",'wb')
+        str = pickle.dumps(net)
+        output_hal.write(str)
+        output_hal.close()
+
     print('Finished Training')
 
-    output_hal = open("test1.pkl",'wb')
-    str = pickle.dumps(net)
-    output_hal.write(str)
-    output_hal.close()
+
 
     print(time.time() - start_time)
